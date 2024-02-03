0e2b6afe066496430921bd40e2470c8e078ed82b
xyntechx <xyntechx@gmail.com>
2024-02-03 00:59:49 -0800

    ✨ Main analysis done

--- app/race/page.tsx
+++ app/race/page.tsx
@@ -5,6 +5,7 @@ import { parse } from "csv-parse";
 import { createClient } from "@/utils/supabase/client";
 import FileSelect from "@/components/FileSelect";
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 
 export default function Race() {
     const supabase = createClient();
@@ -13,6 +14,15 @@ export default function Race() {
     const [fileblob, setFileblob] = useState<Blob | null>(null);
     const [csvColumns, setCSVColumns] = useState<string[]>([]);
     const [positions, setPositions] = useState<string[]>([]);
+
+    const [selectedPosition, setSelectedPosition] = useState<string>("");
+    const [votes, setVotes] = useState<string[][]>([]);
+    const [candidateToCount, setCandidateToCount] = useState<{
+        [key: string]: number;
+    }>({});
+    const [totalVoteCount, setTotalVoteCount] = useState(0);
+
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const downloadCSV = async () => {
@@ -65,6 +75,135 @@ export default function Race() {
         if (csvColumns) getPositionsFromColumns();
     }, [csvColumns]);
 
+    useEffect(() => {
+        const getVotesForPosition = async () => {
+            setIsLoading(true);
+
+            parse(
+                Buffer.from(await fileblob!.arrayBuffer()),
+                {
+                    delimiter: ",",
+                    columns: csvColumns,
+                    fromLine: 2, // exclude first line (headers)
+                    cast: (columnValue, context) => {
+                        if (
+                            context.column.toString().split(" - ")[0] ===
+                            selectedPosition
+                        ) {
+                            return columnValue.split("|")[0].trim();
+                        }
+
+                        return null;
+                    },
+                },
+                (error, result) => {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
+
+                    const relevantVotes = [];
+
+                    for (const line of result) {
+                        const names = [];
+                        for (const col in line) {
+                            if (line[col]) {
+                                names.push(line[col]);
+                            }
+                        }
+                        relevantVotes.push(names);
+                    }
+
+                    setVotes(relevantVotes);
+                    // setTotalVoteCount(relevantVotes.length);
+                }
+            );
+
+            setIsLoading(false);
+        };
+
+        if (selectedPosition && csvColumns && fileblob) getVotesForPosition();
+    }, [selectedPosition, csvColumns, fileblob]);
+
+    useEffect(() => {
+        const currCandidateToCount: { [key: string]: number } = {};
+
+        for (const vote of votes) {
+            if (vote.length === 0) {
+                continue;
+            }
+
+            const candidate = vote[0];
+            if (!Object.keys(currCandidateToCount).includes(candidate)) {
+                currCandidateToCount[candidate] = 0;
+            }
+            currCandidateToCount[candidate]++;
+        }
+
+        const sortedCandidateToCount =
+            sortDescCandidateToCount(currCandidateToCount);
+
+        setCandidateToCount(sortedCandidateToCount);
+        setTotalVoteCount(
+            Object.values(sortedCandidateToCount).reduce(
+                (prev, n) => prev + n,
+                0
+            )
+        );
+    }, [votes]);
+
+    const sortDescCandidateToCount = (obj: { [key: string]: number }) => {
+        let candidateCountArr: any = [];
+
+        for (const candidate in obj) {
+            candidateCountArr.push([candidate, obj[candidate]]);
+        }
+
+        candidateCountArr = candidateCountArr.sort(
+            (a: any, b: any) => b[1] - a[1]
+        );
+
+        console.log(candidateCountArr);
+
+        const result: { [key: string]: number } = {};
+
+        for (const pair of candidateCountArr) {
+            result[pair[0]] = pair[1];
+        }
+
+        return result;
+    };
+
+    const resumeAnalysis = () => {
+        const leastPointCount = Math.min(...Object.values(candidateToCount));
+        const worstCandidates = Object.keys(candidateToCount).filter(
+            (c) => candidateToCount[c] === leastPointCount
+        );
+
+        const newVotes = [...votes];
+
+        if (worstCandidates.length === newVotes.length) {
+            // TODO: Check 3.3.9
+        }
+
+        for (const candidate in candidateToCount) {
+            if (worstCandidates.includes(candidate)) {
+                delete candidateToCount[candidate];
+            }
+        }
+
+        for (let i = 0; i < newVotes.length; i++) {
+            if (worstCandidates.includes(newVotes[i][0])) {
+                newVotes[i] = newVotes[i].slice(1);
+            }
+            newVotes[i] = newVotes[i].filter(
+                (name) => !worstCandidates.includes(name)
+            );
+        }
+
+        setVotes(newVotes);
+    };
+
     return (
         <main className="w-full min-h-screen flex flex-col items-center justify-start gap-y-2">
             <div className="w-full h-fit flex flex-row items-center justify-center gap-x-4">
@@ -74,10 +213,32 @@ export default function Race() {
                 <>
                     <p>Choose position to analyze</p>
                     {positions.map((pos) => (
-                        <Button key={pos}>{pos}</Button>
+                        <Button
+                            key={pos}
+                            onClick={() => setSelectedPosition(pos)}
+                            className="w-[300px]"
+                        >
+                            {pos}
+                        </Button>
                     ))}
                 </>
             )}
+            {isLoading && <p>Loading...</p>}
+            <p>Total Votes: {totalVoteCount}</p>
+            {Object.keys(candidateToCount).map((candidate) => (
+                <div key={candidate} className="w-full">
+                    <p>{candidate}</p>
+                    <Progress
+                        value={
+                            (candidateToCount[candidate] / totalVoteCount) * 100
+                        }
+                    />
+                    <p>
+                        {candidateToCount[candidate]} out of {totalVoteCount}
+                    </p>
+                </div>
+            ))}
+            <Button onClick={() => resumeAnalysis()}>Resume Analysis</Button>
         </main>
     );
 }
